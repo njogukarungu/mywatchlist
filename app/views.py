7aa4b59653dffcecbcd56c98589afdcb3feabf7d
njogukarungu <njogukarungu1@gmail.com>
2020-04-21 23:17:06 +0300

    Added the WTF forms

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from flask import render_template,redirect, url_for, request
 from app import app
 from .request import getMovies, search_movie
+from .models import review
+from .forms import ReviewForm
+Review = review.Review
 
 @app.route('/')
 def index():
@@ -21,3 +24,31 @@ def search(movie_name):
     searched_movie = search_movie(movie_name_format)
     title = {movie_name}
     return render_template('search.html', movies= searched_movie)
+
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = {movie.title} 
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
+
+@app.route('/movie/<int:id>')
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = {movie.title}
+    reviews = Review.get_reviews(movie.id)
+
+    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
+
